90642ac0b9d953937ca5f01e476b19dae2b8d733
unknown <maxwellwijaya00@gmail.com>
2026-03-03 00:15:43 -0800

    adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Landing from "./Landing.tsx";
 
+import { Analytics } from "@vercel/analytics/next";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Landing />
+    <Analytics />
   </StrictMode>,
 );
